8eea2f2c94670f498b87c8ee971b213bb523ce02
JinHao-L <limjinhao@gmail.com>
2020-12-10 12:34:17 +0800

    Clean up code

--- bot.py
+++ bot.py
@@ -14,64 +14,80 @@ import config
 logging.basicConfig(
     format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.INFO
 )
-
 logger = logging.getLogger(__name__)
 
+# Telegram chat id with developer for sending error reports
 DEVELOPER_CHAT_ID = 521610007;
 
-current_pos = (1.304833, 103.831833)
+# Pos of user in (lat, lng) format
+current_pos = None
 
-categories = ReplyKeyboardMarkup([
-    ["Education", "Recreation"],
-    ["Community", "Health"],
-    ["Cultural", "Emergency"],
-    [KeyboardButton(text="📍Update Location", request_location=True)]
-])
+############################# Keyboards #########################################
+def location_prompt_keyboard() -> ReplyKeyboardMarkup:
+    keyboard = [[KeyboardButton(text="📍Share Location", request_location=True)]]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
-cat_education = ReplyKeyboardMarkup([
-    ["Preschools", "Kindergartens"],
-    ["Private Education", "Libraries"],
-    ["CET Centres"],
-    ["All Categories"]
-], one_time_keyboard=True)
+def categories_menu_keyboard() -> ReplyKeyboardMarkup:
+    keyboard = [["Education", "Recreation"],
+                ["Community", "Health"],
+                ["Cultural", "Emergency Services"],
+                [KeyboardButton(text="📍Update My Location", request_location=True)]]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
-cat_recreation = ReplyKeyboardMarkup([
-    ["Tourist Spot", "Hotels"],
-    ["Parks"], ["All Categories"]
-], one_time_keyboard=True)
+def education_submenu_keyboard() -> ReplyKeyboardMarkup:
+    keyboard = [["Preschools", "Kindergartens"],
+                    ["Private Education", "Libraries"],
+                    ["CET Centres"],
+                    ["All Categories"]]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
-cat_community = ReplyKeyboardMarkup([
-    ["Hawker Centres", "Childcare"],
-    ["Supermarkets", "Money Changer"],
-    ["Gyms", "RC", "CC"],
-    ["All Categories"]
-], one_time_keyboard=True)
+def recreation_submenu_keyboard() -> ReplyKeyboardMarkup:
+    keyboard = [["Tourist Spot", "Hotels"],
+                ["Parks"],
+                ["All Categories"]]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
-cat_health = ReplyKeyboardMarkup([
-    ["Pharmacy", "CHAS Clinics"],
-    ["Hospitals"], ["All Categories"]
-], one_time_keyboard=True)
+def community_submenu_keyboard() -> ReplyKeyboardMarkup:
+    keyboard = [["Hawker Centres", "Childcare"],
+                ["Supermarkets", "Money Changer"],
+                ["Gyms", "RC", "CC"],
+                ["All Categories"]]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
-cat_emergency = ReplyKeyboardMarkup([
-    ["Police Station", "Fire Station"],
-    ["AEDs"],
-    ["All Categories"]
-], one_time_keyboard=True)
+def health_submenu_keyboard() -> ReplyKeyboardMarkup:
+    keyboard = [["Pharmacy", "CHAS Clinics"],
+                ["Hospitals"],
+                ["All Categories"]]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
-cat_cultural = ReplyKeyboardMarkup([
-    ["Museums", "Monuments"],
-    ["Historic Sites"],
-    ["All Categories"]
-], one_time_keyboard=True)
 
+def emergency_services_submenu_keyboard() -> ReplyKeyboardMarkup:
+    keyboard = [["Police Station", "Fire Station"],
+                ["AEDs"],
+                ["All Categories"]]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+
+def cultural_submenu_keyboard() -> ReplyKeyboardMarkup:
+    keyboard = [["Museums", "Monuments"],
+                ["Historic Sites"],
+                ["All Categories"]]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+
+############################# Bot #########################################
 def start(update: Update, context: CallbackContext) -> None:
-    # On start
+    """Start bot and prompt for location"""
     username = update.message.from_user.username
     text = "Hello " + username + "!\nLet's find out whats near you.\n\nTo begin share with me your location and select a search query from the categories below\n"
-    update.message.reply_text(text, reply_markup=categories)
+
+    update.message.reply_text(text, reply_markup=location_prompt_keyboard())
 
 def help(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("Hello there! This is a location bot powered by OneMap API.\n\nThis bot provides many useful information on nearby places that are contributed and updated by government agencies.\n\nYou can use /menu or /start to access the main menu. Do share your location before starting a search query!")
+    """Get decription of bot and functionalities"""
+    text = "Hello there! This is a location bot powered by OneMap API.\n\nThis bot provides many useful information, such as the supermarkets, community centres or AEDs near you. These information are contributed and updated by government agencies.\n\nTo begin you can use /menu to access the main menu. Remember to share your latest location with me before starting a search query!\nYou can use /map to get a (night version) map of your current location"
+
+    update.message.reply_text(text)
 
 def error_handler(update: Update, context: CallbackContext) -> None:
     """Log error and send a telegram message to developer."""
@@ -92,48 +108,67 @@ def error_handler(update: Update, context: CallbackContext) -> None:
     context.bot.send_message(chat_id=DEVELOPER_CHAT_ID, text=message, parse_mode=ParseMode.HTML)
     update.message.reply_text("An unexpected error has occured! The developer will be informed")
 
-def mainMenu(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Select a category or update your location', reply_markup=categories)
+def main_menu(update: Update, context: CallbackContext) -> None:
+    """Get the main menu with the categories."""
+    text = "Select a category or update your location"
+    update.message.reply_text(text, reply_markup=categories_menu_keyboard())
 
-def getMyMap(update: Update, context: CallbackContext) -> None :
+def location_prompt(update: Update, context: CallbackContext) -> None:
+    """Prompt the user for a location."""
+    text = "I do not have your location. Share with me your location"
+    update.message.reply_text(text, reply_markup=location_prompt_keyboard())
+
+def share_location(update: Update, context: CallbackContext) -> None:
+    """Update user location"""
+    global current_pos
+    if (update.edited_message) :
+        current_pos = (update.edited_message.location.latitude, update.edited_message.location.longitude)
+    elif (update.message) :
+        current_pos = (update.message.location.latitude, update.message.location.longitude)
+
+    logger.info("Location updated to " + ','.join(map(str, current_pos)))
+    update.message.reply_text('Select a category', reply_markup=categories_menu_keyboard())
+
+def get_nearby_places(update: Update, context: CallbackContext) -> None:
+    """Query for indicated facilities near the user"""
+    if (current_pos == None) :
+        # prompt for location
+        return location_prompt(update, context)
+
+    result = onemap.get_nearby_places(update.message.text, current_pos[0], current_pos[1])
+    update.message.reply_text(result, parse_mode=ParseMode.HTML)
+
+def get_my_map(update: Update, context: CallbackContext) -> None :
+    """Get map of user"""
+    if (current_pos == None) :
+        # prompt for location
+        return location_prompt(update, context)
+
     url = onemap.getMapUrl(current_pos[0], current_pos[1])
     logger.info("Showing img: " + url)
     update.message.bot.send_photo(chat_id = update.message.chat.id, photo=url)
 
-def updateLocation(update: Update, context: CallbackContext) -> None:
-    """Update location"""
-    global current_pos
-    current_pos = (update.message.location.latitude, update.message.location.longitude)
-    logger.info("Location updated to " + ','.join(map(str, current_pos)))
-
-def getNearbyPlaces(update: Update, context: CallbackContext) -> None:
-    """Get nearby"""
-    result = onemap.getNearbyPlaces(update.message.text, current_pos[0], current_pos[1])
-    update.message.reply_text(result, parse_mode=ParseMode.HTML)
-
-
-
-def getCategory(update: Update, context: CallbackContext) -> None:
+def get_category(update: Update, context: CallbackContext) -> None:
+    """Get 2nd level keyboard choices"""
     message = update.message.text;
     if (message == "Education") :
-        reply_choice = cat_education
+        reply_choice = education_submenu_keyboard()
     elif (message == "Recreation") :
-        reply_choice = cat_recreation
+        reply_choice = recreation_submenu_keyboard()
     elif (message == "Community") :
-        reply_choice = cat_community
+        reply_choice = community_submenu_keyboard()
     elif (message == "Health") :
-        reply_choice = cat_health
-    elif (message == "Emergency") :
-        reply_choice = cat_emergency
+        reply_choice = health_submenu_keyboard()
+    elif (message == "Emergency Services") :
+        reply_choice = emergency_services_submenu_keyboard()
     elif (message == "Cultural") :
-        reply_choice = cat_cultural
+        reply_choice = cultural_submenu_keyboard()
     else :
         raise Exception("No such category found")
 
     update.message.reply_text("Select a search query", reply_markup=reply_choice)
 
-
-
+############################# Main #########################################
 def main():
     """Start the bot"""
     # Create the Updater with bot's token
@@ -144,25 +179,25 @@ def main():
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
-    # on different commands - answer in telegram
+    # Command handler
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("menu", mainMenu))
-    dispatcher.add_handler(CommandHandler("map", getMyMap))
+    dispatcher.add_handler(CommandHandler("menu", main_menu))
+    dispatcher.add_handler(CommandHandler("map", get_my_map))
     dispatcher.add_handler(CommandHandler("help", help))
-    # dispatcher.add_handler(CommandHandler("where", getCurrLocation))
 
-    dispatcher.add_handler(MessageHandler(Filters.regex('^(Education|Recreation|Community|Health|Emergency|Cultural)$'),getCategory))
-    dispatcher.add_handler(MessageHandler(Filters.regex('^(All Categories)$'),mainMenu))
+    # Message handler
+    dispatcher.add_handler(MessageHandler(Filters.regex('^(Education|Recreation|Community|Health|Emergency Services|Cultural)$'),get_category))
+    dispatcher.add_handler(MessageHandler(Filters.regex('^(All Categories)$'),main_menu))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, get_nearby_places))
+    dispatcher.add_handler(MessageHandler(Filters.location, share_location))
 
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, getNearbyPlaces))
-    dispatcher.add_handler(MessageHandler(Filters.location, updateLocation))
-
-    # error handler
+    # Error handler
     dispatcher.add_error_handler(error_handler)
 
-    # startBot
-    updater.start_webhook(listen = "0.0.0.0", port = PORT, url_path = config.BOT_TOKEN)
-    updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, config.BOT_TOKEN))
+    # Start bot
+    updater.start_polling();
+    # updater.start_webhook(listen = "0.0.0.0", port = PORT, url_path = config.BOT_TOKEN)
+    # updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, config.BOT_TOKEN))
 
     # Run the bot until bot stopped
     updater.idle()
--- onemap.py
+++ onemap.py
@@ -9,8 +9,8 @@ from datetime import datetime
 
 import config
 
-# theme name -> theme code
-themesMap = {
+"""Map of theme names to theme codes (based on OneMap API)"""
+themesmap = {
     "Preschools": "preschools_location",
     "Kindergartens": "kindergartens",
     "Private Education": "cpe_pei_premises",
@@ -42,28 +42,44 @@ themesMap = {
     "Historic Sites" : "historicsites",
 }
 
-def getAccessToken(email : str, password : str) -> str:
+"""Theme names that contains useful descriptions"""
+usefuldescriptions = {
+    "Tourist Spot",
+    "Parks", "Gyms",
+    "CHAS Clinics",
+    "Museums",
+    "Monuments",
+    "Historic Sites",
+    "Fire Station"
+ }
+
+
+def get_access_token(email : str, password : str) -> str:
+    """Retrieve access token"""
     dt = datetime.now()
     timestamp = dt.timestamp()
+
+    # refresh access token if expired
     if (config.expiry_timestamp < timestamp) :
-        # refresh access token
         logging.info("Updating access token")
         loginCredentials = {"email": email, "password": password}
+        url = "https://developers.onemap.sg/privateapi/auth/post/getToken"
 
-        response = requests.post("https://developers.onemap.sg/privateapi/auth/post/getToken", loginCredentials)
+        response = requests.post(url, loginCredentials)
         result = response.json()
 
         if (response.status_code == 200) :
-            logging.info("Token expiry on " + result["expiry_timestamp"])
-            updateConfigFile(result["access_token"], int(result["expiry_timestamp"]))
+            logging.info("New token expiry on " + result["expiry_timestamp"])
+            # update config file with new token
+            update_config_file(result["access_token"], int(result["expiry_timestamp"]))
             return result["access_token"]
         else :
             raise Exception(result["error"])
 
-
     return config.access_token
 
-def updateConfigFile(token: str, timestamp: int) -> None :
+def update_config_file(token: str, timestamp: int) -> None :
+    """Read config.py file and update it with new token"""
     lines = None
     with open("config.py", 'r') as cfgr:
         lines = cfgr.readlines()
@@ -80,31 +96,32 @@ def updateConfigFile(token: str, timestamp: int) -> None :
     logging.info("Config file successfully updated with new access token")
     importlib.reload(config)
 
-def getNearbyPlaces(searchTerm: str, lat : float, lng: float) -> str:
-
-    if (searchTerm not in themesMap) :
+def get_nearby_places(searchterm: str, lat : float, lng: float) -> str:
+    """Get top 8 places near (lat, lng) as a String"""
+    if (searchterm not in themesmap) :
         return "This search theme is currently not available"
 
-    theme = themesMap[searchTerm]
+    # Get theme code from theme name
+    theme = themesmap[searchterm]
 
-    token = getAccessToken(config.email, config.password)
+    token = get_access_token(config.email, config.password)
 
-    queryString = {"queryName": theme, "token" : token, "extents" : getBoundary(lat, lng)}
+    queryString = {"queryName": theme, "token" : token, "extents" : get_bounding_box(lat, lng, 2.5)}
 
     url = "https://developers.onemap.sg/privateapi/themesvc/retrieveTheme"
     response = requests.get(url, params=queryString)
     result = json.loads(response.text)
 
     if (response.status_code == 200) :
-        return parsePlaces(result, (lat, lng), searchTerm)
+        return parse_places(result, (lat, lng), searchterm)
     else :
         logging.error(str(response.status) + ", " + response.text)
         return result["error"]
 
 
-def getBoundary(lat: float, lng: float) -> str:
-    # radius of 2.5 km
-    offset = 2.5 / 100.0
+def get_bounding_box(lat: float, lng: float, radius: float) -> str:
+    """Get bounding box with length of radius * 2 and (lat, lng) as the centre"""
+    offset = radius / 100.0
     latMax = lat + offset
     latMin = lat - offset
 
@@ -115,72 +132,80 @@ def getBoundary(lat: float, lng: float) -> str:
     result = str(latMin) + "," + str(lngMin) + "," + str(latMax) + "," + str(lngMax)
     return result
 
-def getDistance(pos1: (float, float), pos2: (float, float)) -> float :
+def get_distance(pos1: (float, float), pos2: (float, float)) -> float :
+    """Get distance between two position with format (lat, lng)"""
     return distance.distance(pos1, pos2).km
 
-usefulDescriptions = {"Tourist Spot", "Parks", "Gyms", "CHAS Clinics", "Museums", "Monuments", "Historic Sites", "Fire Station"}
-def parsePlaces(response : json, pos: (float, float), name : str) -> str:
+def parse_places(response : json, pos: (float, float), name : str) -> str:
+    """Parse json response to output string of the top 8 places"""
     places = response["SrchResults"]
+
     if ("ErrorMessage" in places[0]) :
+        logging.error("Invalid theme code for " + name)
         return "Search term not available"
 
     logging.info("Found " + str(places[0]["FeatCount"]) + " results for " + name)
+
     if (places[0]["FeatCount"] == 0) :
+        # No result
         return "No " + name.lower() + " found in a ~2.5km radius"
 
     output = []
     for place in places[1:]:
         try :
-            distance = getDistance(pos, place["LatLng"].split(","))
+            distance = get_distance(pos, place["LatLng"].split(","))
 
-            resultString = ""
-            resultString = "<b>{}</b> ({:.2f}km)\n".format(place["NAME"].title(), distance)
+            resultstring = ""
+            resultstring = "<b>{}</b> ({:.2f}km)\n".format(place["NAME"].title(), distance)
 
-            if ("DESCRIPTION" in place and name in usefulDescriptions) :
+            if ("DESCRIPTION" in place and name in usefuldescriptions) :
                 if (name == "CHAS Clinics") :
-                    resultString += "<i>{}</i>\n---\n".format(place["DESCRIPTION"].replace(",", ", "))
+                    resultstring += "<i>{}</i>\n---\n".format(place["DESCRIPTION"].replace(",", ", "))
                 else :
-                    resultString += "<i>{}</i>\n---\n".format(place["DESCRIPTION"])
+                    resultstring += "<i>{}</i>\n---\n".format(place["DESCRIPTION"])
 
             if ("ADDRESSBLOCKHOUSENUMBER" in place and place["ADDRESSBLOCKHOUSENUMBER"]) :
-                resultString += "Blk " + place["ADDRESSBLOCKHOUSENUMBER"].strip().upper().replace("BLK", "").replace("BLOCK", "") + ", "
+                resultstring += "Blk " + place["ADDRESSBLOCKHOUSENUMBER"].strip().upper().replace("BLK", "").replace("BLOCK", "") + ", "
 
             if ("ADDRESSSTREETNAME" in place and place["ADDRESSSTREETNAME"]) :
-                resultString += place["ADDRESSSTREETNAME"].strip().title() + ", "
+                resultstring += place["ADDRESSSTREETNAME"].strip().title() + ", "
 
             if ("ADDRESSUNITNUMBER" in place and place["ADDRESSUNITNUMBER"]) :
-                resultString += place["ADDRESSUNITNUMBER"].strip() + ", "
+                resultstring += place["ADDRESSUNITNUMBER"].strip() + ", "
 
             if ("ADDRESSBUILDINGNAME" in place and place["ADDRESSBUILDINGNAME"]) :
-                resultString += place["ADDRESSBUILDINGNAME"].strip().title() + ", "
+                resultstring += place["ADDRESSBUILDINGNAME"].strip().title() + ", "
 
             if ("ADDRESSPOSTALCODE" in place and place["ADDRESSPOSTALCODE"]) :
-                resultString += "S" + place["ADDRESSPOSTALCODE"]
+                resultstring += "S" + place["ADDRESSPOSTALCODE"]
 
-            if (resultString.endswith(", ")) :
-                resultString = resultString[:-2] + "\n"
-            elif (not resultString.endswith("\n")) :
-                resultString += "\n"
+            if (resultstring.endswith(", ")) :
+                resultstring = resultstring[:-2] + "\n"
+            elif (not resultstring.endswith("\n")) :
+                resultstring += "\n"
 
             if ("HYPERLINK" in place and place["HYPERLINK"]) :
-                resultString += "[<a href=\"{}\">Link</a>] ".format(place["HYPERLINK"])
+                resultstring += "[<a href=\"{}\">Link</a>] ".format(place["HYPERLINK"])
 
-            resultString += "[<a href=\"http://www.google.com/maps/place/{}\">Map</a>] ".format(place["LatLng"])
+            resultstring += "[<a href=\"http://www.google.com/maps/place/{}\">Map</a>] ".format(place["LatLng"])
 
-            output.append((resultString + "\n", distance))
+            output.append((resultstring + "\n", distance))
         except Exception as e:
             logging.warning(e)
             continue
 
+    # Sort results by distance from pos
     output = sorted(output, key = lambda place : place[1])
 
-    resultString = ""
-    # display only top 8 results
+    # Get only top 8 results
+    top8 = ""
     for idx, place in enumerate(output[: 8]):
-        resultString += str(idx + 1) + ". " + place[0] + "\n"
+        top8 += str(idx + 1) + ". " + place[0] + "\n"
 
-    return resultString
+    return top8
 
 def getMapUrl(lat: float, lng: float) :
+    """Get static map url based on position"""
     url = 'https://developers.onemap.sg/commonapi/staticmap/getStaticImage?layerchosen={0}&lat={1}&lng={2}&zoom={3}&height={4}&width={5}'
+    # default of map type "night", zoom 17, size of (512, 512)
     return url.format("night", lat, lng, 17, 512, 512);
